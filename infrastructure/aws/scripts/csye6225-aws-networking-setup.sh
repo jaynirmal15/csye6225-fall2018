89b84e10be9617aaaaecd9ec2f9de7ed7c06e4b1
Ronak Shingala <shingala.r@husky.neu.edu>
2018-09-24 20:09:30 -0400

    create subnet test

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -8,13 +8,13 @@ read STACK_NAME
 echo "STACK_NAME entered: " $STACK_NAME
 
 # Get VPC id
-vpc_id=$(aws ec2 describe-vpcs --query 'Vpcs[0].[VpcId]' --output text)
-echo $vpc_id
+#vpc_id=$(aws ec2 describe-vpcs --query 'Vpcs[0].[VpcId]' --output text)
+#echo $vpc_id
 
 # Get availability zones
 # avail_zones=$(aws ec2 describe-availability-zones)
 # echo $avail_zones
 
 # Create Subnet
-aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.1.0/24
+aws ec2 create-subnet --vpc-id vpc-0be70c10aa459c235 --cidr-block 10.0.2.0/24
 
