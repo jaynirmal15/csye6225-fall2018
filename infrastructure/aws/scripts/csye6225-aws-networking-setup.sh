1725108e47555861e562dfb17e72445b62113685
supriya <bhide.su@husky.neu.edu>
2018-09-25 07:07:14 +0530

    Create route table and route

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -18,10 +18,19 @@ echo "STACK_NAME entered: " $STACK_NAME
 
 # Create Subnet
 # aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.1.0/24
-aws ec2 create-subnet --vpc-id vpc-0be70c10aa459c235 --cidr-block 10.0.2.0/24
+#aws ec2 create-subnet --vpc-id vpc-0be70c10aa459c235 --cidr-block 10.0.2.0/24
 
 # Create Subnet 3
-aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.3.0/24
+#aws ec2 create-subnet --vpc-id $new_vpc --cidr-block 10.0.3.0/24
+
+
+# Create Route Table
+new_rt_id=$(aws ec2 create-route-table --vpc-id $new_vpc --query 'RouteTable.[RouteTableId]' --output text)
+echo $new_rt_id
+
+#new_route=$(aws ec2 create-route --route-table-id $new_rt_id --destination-cidr-block 0.0.0.0/0 --gateway-id $new_ig_id)
+
+
 
 
 
