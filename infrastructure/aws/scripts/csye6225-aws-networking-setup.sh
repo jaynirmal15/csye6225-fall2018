7dfaeb58bc659f49def290593ef77b4a3ca22044
supriya <bhide.su@husky.neu.edu>
2018-09-25 05:40:43 +0530

    get newly added vpc id

--- infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-setup.sh
@@ -1,20 +1,21 @@
 # Create VPC with CLI command
 
 echo "Creating VPC..."
-# aws ec2 create-vpc --cidr-block 10.0.0.0/16
+new_vpc=$(aws ec2 create-vpc --cidr-block 10.0.0.0/16 --query 'Vpc.[VpcId]' --output text)
+echo $new_vpc
 
 echo "Enter STACK_NAME: "
 read STACK_NAME
 echo "STACK_NAME entered: " $STACK_NAME
 
 # Get VPC id
-vpc_id=$(aws ec2 describe-vpcs --query 'Vpcs[0].[VpcId]' --output text)
-echo $vpc_id
+#vpc_id=$(aws ec2 describe-vpcs --query 'Vpcs[0].[VpcId]' --output text)
+#echo $vpc_id
 
 # Get availability zones
 # avail_zones=$(aws ec2 describe-availability-zones)
 # echo $avail_zones
 
 # Create Subnet
-aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.1.0/24
+# aws ec2 create-subnet --vpc-id $vpc_id --cidr-block 10.0.1.0/24
 
