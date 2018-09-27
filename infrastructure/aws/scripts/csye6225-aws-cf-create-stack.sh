7f3bbec0ddfa558efd3b02d7b041089f67b8035e
supriya <bhide.su@husky.neu.edu>
2018-09-27 07:53:42 +0530

    Add Route table, Route to json; Modify subnets for public-private category; Associate only public subnets

--- infrastructure/aws/scripts/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/scripts/csye6225-aws-cf-create-stack.sh
@@ -1,16 +1,25 @@
 echo "Please Enter a Name for the stack: "
 read stack_name
 
+#VPC Name
 jq '.Resources.myVPC.Properties.Tags[0].Value = "'$stack_name'-csye6225-vpc"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
+#Subnets
+jq '.Resources.publicsubnet1.Properties.Tags[0].Value = "'$stack_name'-public-subnet-1"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
 
-jq '.Resources.subnet1.Properties.Tags[0].Value = "'$stack_name'-csye6225-subnet1"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
+jq '.Resources.privatesubnet1.Properties.Tags[0].Value = "'$stack_name'-private-subnet-1"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
 
-jq '.Resources.subnet2.Properties.Tags[0].Value = "'$stack_name'-csye6225-subnet2"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
+jq '.Resources.publicsubnet2.Properties.Tags[0].Value = "'$stack_name'-public-subnet-2"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
 
-jq '.Resources.subnet3.Properties.Tags[0].Value = "'$stack_name'-csye6225-subnet3"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
+jq '.Resources.privatesubnet2.Properties.Tags[0].Value = "'$stack_name'-private-subnet-2"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
 
+jq '.Resources.publicsubnet3.Properties.Tags[0].Value = "'$stack_name'-public-subnet-3"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
 
+jq '.Resources.privatesubnet3.Properties.Tags[0].Value = "'$stack_name'-private-subnet-3"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
+#Internet Gateway
 jq '.Resources.myInternetGateway.Properties.Tags[0].Value = "'$stack_name'-csye6225-InternetGateway"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
+#Route Table
+jq '.Resources.myRouteTable.Properties.Tags[0].Value = "'$stack_name'-csye6225-public-route-table"' ../cloudformation/csye6225-cf-networking.json > tmp.$$.json && mv tmp.$$.json ../cloudformation/csye6225-cf-networking.json
+
 
 echo "Executing Create Stack....."
 
