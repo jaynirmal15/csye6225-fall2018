56e9ee51c436831b84353d8db3bcc7163c90d623
supriya <bhide.su@husky.neu.edu>
2018-09-27 14:13:16 +0530

    Add error validation for stack creation; Add CLI route delete functionality

--- infrastructure/aws/scripts/csye6225-aws-cf-create-stack.sh
+++ infrastructure/aws/scripts/csye6225-aws-cf-create-stack.sh
@@ -25,7 +25,20 @@ echo "Executing Create Stack....."
 
 aws cloudformation create-stack --stack-name ${stack_name} --template-body file://../cloudformation/csye6225-cf-networking.json --capabilities=CAPABILITY_NAMED_IAM
 
+if [ $? -eq 0 ]; then
+	echo "Waiting to create gets executed completely...!"
+else
+	echo "Error in Create Stack...Exiting..."
+	exit 1
+fi
 
 aws cloudformation wait stack-create-complete --stack-name ${stack_name}
+
+if [ $? -eq 0 ]; then
+	echo "Create successfully executed...!"
+else
+	echo "Error in Create Stack...Exiting..."
+	exit 1
+fi
 
 echo "Stack Create Execution Complete...!!!"
--- infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
+++ infrastructure/aws/scripts/csye6225-aws-networking-teardown.sh
@@ -13,15 +13,26 @@ echo " Internet Gateway Delete - Press 3"
 echo " Route Table Delete - Press 4"
 read input
 
-if [$input == "1"]; then
+if [ $input == "1" ]; then
 	echo " Deleting Vpc that you entered the name....."
-elif [$input == "2"]; then
+elif [ $input == "2" ]; then
 	echo " Please select the subnetId to be deleted..."
 	aws ec2 describe-subnets --filters Name=vpc-id,Values=${vpc_id} | jq -r '.Subnets[].SubnetId'
-elif [$input == "3"]; then
+elif [ $input == "3" ]; then
 	echo
-elif [$input == "4"]; then
-	echo
+elif [ $input == "4" ]; then
+	echo "fetching Route table"
+	aws ec2 describe-route-tables --filters Name=vpc-id,Values=$vpcId | jq -r '.RouteTables[] | select(.Tags[].Key == "Name").Tags[].Value,.RouteTableId'
+	echo "Enter route table ID to delete: "
+	read rtId
+	aws ec2 delete-route-table --route-table-id $rtId
+	if [ $? -eq 0 ]; then
+		echo "Route table deleted successfully...!"
+	else
+		echo "Error in deletion..."
+		exit 1
+	fi
+
 else
 	echo "Invalid input detected....."
 	exit 1
