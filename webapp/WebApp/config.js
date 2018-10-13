267cec320a029a897081b1fe44e783809c4c3c71
supriya <bhide.su@husky.neu.edu>
2018-10-12 21:49:19 -0400

    remove values from config file...commit only structure

--- webapp/WebApp/config.js
+++ webapp/WebApp/config.js
@@ -1,9 +1,6 @@
 const config = {
 	aws : {
-		ec2_keyName: "supriya-ec2-key",
-		accountId: "359938524153",
-		bucket_image: "csye6225-fall2018-bhidesu.me.tld.csye6225.com",
-		domainName: "csye6225-fall2018-bhidesu.me"
+
 	}
 };
 
