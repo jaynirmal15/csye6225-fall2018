2b4de22061fb36289807f199c12fb5dc77eebf27
Ronak Shingala <shingala.r@husky.neu.edu>
2018-10-22 20:30:10 -0400

    changes added..

--- infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
@@ -8,9 +8,9 @@ read nw_stack_name
 echo "Enter the DynamoDB table name"
 read dynamoDB_table
 
-bucket_name="csye6225-fall2018-sawale.me.tld.csye6225.com"
-dbidentifier="psawale-sye6225-fall2018"
-dBsubnetGroup_name="psawale-dbSubnetGrp"
+bucket_name="csye6225-fall2018-shingalar.me.tld.csye6225.com"
+dbidentifier="rshingala-csye6225-fall2018"
+dBsubnetGroup_name="rshingala-dbSubnetGrp"
 
 vpc_id=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='aws:cloudformation:stack-name']|[?Value=='$nw_stack_name']].VpcId" --output text)
 echo "VPC ID: " $vpc_id
