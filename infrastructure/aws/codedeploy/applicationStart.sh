7ccd2bf5228a5c7fd75f7c8bd8c24d508b4c5b84
psawale <prashant@localhost.localdomain>
2018-10-25 02:36:46 -0400

    fix

--- infrastructure/aws/codedeploy/applicationStart.sh
+++ infrastructure/aws/codedeploy/applicationStart.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-cd var
-pwd
-ls -a
 echo "#CSYE6225: start application pwd and move into nodeapp dir"
 pwd
-cd ./webapp/WebApp
+cd var/webapp/WebApp/
 echo "PWD AND FILES"
 pwd
 ls -lrt
-pm2 start ./index.js
+pm2 start index.js
 
