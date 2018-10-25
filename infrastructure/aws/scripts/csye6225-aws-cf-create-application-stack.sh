a9586a40c668a10be05946dc2c1d03398fe83347
psawale <prashant@localhost.localdomain>
2018-10-24 21:59:30 -0400

    commit for travis

--- infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
@@ -19,9 +19,9 @@ read nw_stack_name
 echo "Enter the DynamoDB table name"
 read dynamoDB_table
 
-bucket_name="csye6225-fall2018-nirmalj.me.tld.csye6225.com1"
-dbidentifier="psawale-sye6225-fall20181"
-dBsubnetGroup_name="psawale-dbSubnetGrp1"
+bucket_name="csye6225-fall2018-sawalep.me.tld.csye6225.com2"
+dbidentifier="psawale-sye6225-fall20182"
+dBsubnetGroup_name="psawale-dbSubnetGrp2"
 
 domain=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
 trimdomain=${domain::-1}
