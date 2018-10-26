334d9e3feabc8db7307b5ba9a40e79b15264704a
Jay Nirmal <nirmal.j@husky.neu.edu>
2018-10-25 22:17:41 -0400

    testing testing

--- infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
@@ -19,7 +19,7 @@ read nw_stack_name
 echo "Enter the DynamoDB table name"
 read dynamoDB_table
 
-bucket_name="csye6225-fall2018-bhidesu.me.tld.csye6225.com"
+bucket_name="csye6225-fall2018-nirmalj.me.tld.csye6225.com"
 dbidentifier="bhidesu-csye6225-fall2018"
 dBsubnetGroup_name="csye6225-dbSubnetGrp"
 
