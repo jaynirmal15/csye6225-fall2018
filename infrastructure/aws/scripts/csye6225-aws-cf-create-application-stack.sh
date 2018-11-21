0833226816874adf776d7c4998c53fdcc89bdade
Ronak Shingala <shingala.r@husky.neu.edu>
2018-11-20 19:23:00 -0500

    Revert "code review asn5-6"
    This reverts commit 53f1f136c25cf95df5a31d7b37fffdff3318d7d9.

--- infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
+++ infrastructure/aws/scripts/csye6225-aws-cf-create-application-stack.sh
@@ -19,7 +19,7 @@ read nw_stack_name
 echo "Enter the DynamoDB table name"
 read dynamoDB_table
 
-bucket_name="csye6225-fall2018-shingalar.me.tld.csye6225.com"
+bucket_name="csye6225-fall2018-nirmalj.me.tld.csye6225.com"
 dbidentifier="rshingala-csye6225-fall2018"
 dBsubnetGroup_name="rshingala-dbSubnetGrp"
 
