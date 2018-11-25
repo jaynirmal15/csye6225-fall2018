b236b1caef4272845d508dea60857cd2d0d57909
psawale <prashant@localhost.localdomain>
2018-11-25 02:29:54 -0500

    loadbalenser

--- infrastructure/aws/scripts/csye6225-aws-cf-create-auto-scaling-application-stack.sh
+++ infrastructure/aws/scripts/csye6225-aws-cf-create-auto-scaling-application-stack.sh
@@ -47,11 +47,12 @@ echo "SSLArn: $SSLArn"
 vpc_id=$(aws ec2 describe-vpcs --query "Vpcs[?Tags[?Key=='aws:cloudformation:stack-name']|[?Value=='$nw_stack_name']].VpcId" --output text)
 echo "VPC ID: " $vpc_id
 
-subnet_id_pub=$(aws ec2 describe-subnets --query "Subnets[?Tags[?contains(Value, 'public')]] | [0].SubnetId " --output text)
+#subnet_id_pub=$(aws ec2 describe-subnets --query "Subnets[?Tags[?contains(Value, 'public')]] | [0].SubnetId " --output text)
+subnet_id_pub=$(aws ec2 describe-route-tables --query "RouteTables[?Tags[?Key=='Name']|[?Value=='$nw_stack_name-csye6225-public-route-table']].Associations[].SubnetId")
 #retrieve subnet_ids from the existing created vpc using vpc_id and stack name
 ###################################################################################
 
-echo "Subnet ID: " $subnet_id
+echo "Subnet ID: " $subnet_id_pub
 
 
 ###################################################################################
@@ -172,7 +173,7 @@ echo "Executing Create Stack....."
 
 aws cloudformation create-stack --stack-name ${stack_name} --template-body file://../cloudformation/csye6225-cf-auto-scaling-application.json --capabilities=CAPABILITY_NAMED_IAM --parameters ParameterKey=SSLArn,ParameterValue=$SSLArn ParameterKey=VpcId,ParameterValue=$vpc_id ParameterKey=senderEmail,ParameterValue=$senderEmail ParameterKey=dynamoDB,ParameterValue=$dynamoDB_table ParameterKey=s3domain,ParameterValue=$bucket_name ParameterKey=myAccountId,ParameterValue=$accid ParameterKey=DBSubnetGroupName,ParameterValue=$dBsubnetGroup_name ParameterKey=DBInstanceIdentifier,ParameterValue=$dbidentifier ParameterKey=HostedZoneName,ParameterValue=$domain
 
-if eq [ $? -if 0 ]; then
+if [ $? -eq 0 ]; then
 	echo "Waiting to create gets executed completely...!"
 else
 	echo "Error in Create Stack...Exiting..."
